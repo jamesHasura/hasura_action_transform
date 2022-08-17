552380005c65b9de43de5f6483e6a18cca4b4c1c
jamesHasura <james@hasura.io>
2022-08-17 06:21:27 -0400

    second

---
+++ data-api/prettier.config.js
@@ -0,0 +1,20 @@
+// prettier.config.js
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSameLine: false,
+  bracketSpacing: false,
+  embeddedLanguageFormatting: 'auto',
+  htmlWhitespaceSensitivity: 'css',
+  insertPragma: false,
+  jsxSingleQuote: false,
+  printWidth: 80,
+  proseWrap: 'always',
+  quoteProps: 'as-needed',
+  requirePragma: false,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+  vueIndentScriptAndStyle: false,
+};
